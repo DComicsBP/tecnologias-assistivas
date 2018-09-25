903aaa0755bc7787afb2467e27fe0a78ff988d6c
DComicsBP <dayonne.p@gmail.com>
2018-09-25 02:08:56 -0300

    renderizando as coisas do json

--- Components/HtmlComponnents/Jogo-Animais/Animais.js
+++ Components/HtmlComponnents/Jogo-Animais/Animais.js
@@ -1,16 +1,28 @@
 var Animais =  ()=>{
     console.log('Teste Animais III')
 }
+var questions = [];
+var question = "Teste maroto"
+
 function loadGames() {
-  
-    var saida = '';
-    
-    var bands = [];
-    
     $.getJSON("./info-jogo.json", function(data) {
-      
-      console.log("DATA ===>", data);
+        data.forEach(element => {
+          questions.push(element)
+  
+      });
+      document.getElementById("title").innerHTML = data[1].Title; 
     });
   }
-  
+  debugger; 
   loadGames();
+  console.log(questions)
+  question = questions.toString();
+  $(document).ready(function() {
+    $('.gallerythumbnail').on('click', function() {
+        var img = $('<img />', {src    : '/Assets/Image/Animais/baleiaJubarte.png',
+                                'class': 'fullImage'
+                  });
+
+        $('.showimagediv').html(img).show();
+    });
+});
