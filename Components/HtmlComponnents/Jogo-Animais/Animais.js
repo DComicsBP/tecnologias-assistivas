27263b79aea34c7eb7241ed7c517c26cde0ecfbc
DaioneB <dcbpavan@restinga.ifrs.edu.br>
2018-09-25 09:03:27 -0300

    mais um commit

--- Components/HtmlComponnents/Jogo-Animais/Animais.js
+++ Components/HtmlComponnents/Jogo-Animais/Animais.js
@@ -1,28 +1,60 @@
-var Animais =  ()=>{
-    console.log('Teste Animais III')
-}
+var Animais = ()=>{
+
+
 var questions = [];
 var question = "Teste maroto"
+var imageSrc = null; 
+var opcaoA; 
+var opcaoB;
+var title = null;  
+window.onload = function(){
+    loadGames();
+}
 
 function loadGames() {
     $.getJSON("./info-jogo.json", function(data) {
         data.forEach(element => {
           questions.push(element)
-  
       });
-      document.getElementById("title").innerHTML = data[1].Title; 
+      console.log(questions)
+
+      title = data[0].Title; 
+      opcaoA= data[0].Opcoes.opA
+      opcaoB = data[0].Opcoes.opB
+        $("#principalImage").attr("src", function(){
+            this.src = data[0].Imagem
+            console.log(this); 
+            debugger;   
+        }); 
+
+        $("#titleA").attr("id", function(){
+            console.log('OpA ==> ',opcaoA.Imagem)
+            this.src = opcaoA.NomeObjeto
+        });
+
+        $("#titleB").attr("id", function(){
+            this.innerHTML = opcaoB.NomeObjeto
+
+        });
+
+        $("#opA").attr("id", function(){
+            // console.log('OpA IMAGEM ==> ', opcaoA.Imagem);
+            const src = opcaoA.Imagem;
+            // console.log('SRC --> ', src);
+            // console.log('opA ==> ', this)
+            this.src = src;
+        })
+        $("#opB").attr("id", function(){
+            // console.log('opB ==> ', this)
+            // console.log('OPC B --> ', opcaoB.Imagem);
+            const src = opcaoB.Imagem
+            this.src = src;
+            
+        })
+
     });
   }
-  debugger; 
-  loadGames();
-  console.log(questions)
-  question = questions.toString();
-  $(document).ready(function() {
-    $('.gallerythumbnail').on('click', function() {
-        var img = $('<img />', {src    : '/Assets/Image/Animais/baleiaJubarte.png',
-                                'class': 'fullImage'
-                  });
+}
 
-        $('.showimagediv').html(img).show();
-    });
-});
+Animais();
+
