bc60f4e4e6d67af1f140a9791830160ba712d48e
DaioneB <dcbpavan@restinga.ifrs.edu.br>
2018-10-20 13:23:20 -0300

    adicionando o json das  rotinas

---
+++ Components/HtmlComponnents/Rotinas-I/jogo-items.js
@@ -0,0 +1,34 @@
+var itens = [
+    {
+        "ID": "JOGO01",
+        "Imagem01": "",
+        "Imagem02": "",
+        "Imagem03": "",
+        "Imagem04": "",
+        "opcoes": [
+            {
+                "opA": [
+
+                    { "src": "" },
+                    { "flag": "" },
+                    { "width": "" },
+                    { "height": "" },
+                    { "narracao": "" },
+
+                ]
+            },
+            {
+                "opA": [
+
+                    { "src": "" },
+                    { "flag": "" },
+                    { "width": "" },
+                    { "height": "" },
+                    { "narracao": "" },
+
+                ]
+            },
+
+        ]
+    },
+]
