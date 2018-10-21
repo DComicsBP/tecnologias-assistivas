30f2427a689d8b797b9decc2eb801bb7173e5ff0
DaioneB <dcbpavan@restinga.ifrs.edu.br>
2018-10-21 16:31:47 -0200

    um commit

---
+++ Components/HtmlComponnents/Jogo-Estacoes/estacaoInverno/jogo-itens.js
@@ -0,0 +1,110 @@
+var itens = [
+    {
+      "ID":"Jogo01",
+      "Imagem":"../../../../Assets/Image/Estacoes/imagem_sol.jpeg",
+      "Width": 200, 
+      "Height":300,
+      "Title":"QUAL ROUPA SE USA NO VERÃO?",
+      "Pergunta": "../../../../Assets/Audio/Estacoes/Jogo01/file.mp3", 
+      "Opcoes":{
+        "opA":{
+          "NomeObjeto":"A) CAMISETA 1", 
+          "Imagem": "../../../../Assets/Image/Estacoes/camiseta1.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/Jogo01/file.mp3",
+          "Flag": true, 
+          "width":160, 
+          "height":160
+        }, 
+        "opB":{
+          "NomeObjeto":"B) BLUSÃO", 
+          "Imagem": "../../../../Assets/Image/Estacoes/jaqueta.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/Jogo01/file.mp3",
+          "Flag": false, 
+          "width":160, 
+          "height":160
+        }, 
+        "opC":{
+          "NomeObjeto":" C) CAMISETA 2", 
+          "Imagem": "../../../../Assets/Image/Estacoes/jaqueta1.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/Jogo01/file.mp3",
+          "Flag": false,
+          "width":160, 
+          "height":160
+        }
+  
+      }
+    },
+     
+    {
+      "ID":"Jogo02",
+      "Imagem":"../../../../Assets/Image/Estacoes/chuva.jpg",
+      "Width": 100, 
+      "Height":250,
+      "Title":"O QUE DEVEMOS USAR PARA SE PREOTEGER DA CHUVA?",
+      "Pergunta": "../../../../Assets/Audio/Estacoes/Jogo02/questao.mp3", 
+      "Opcoes":{
+        "opA":{
+          "NomeObjeto":"OPÇÃO A", 
+          "Imagem": "../../../../Assets/Image/Estacoes/guarda_chuva.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/Jogo02/opA.mp3",
+          "Flag": true, 
+          "width":200, 
+          "height":200
+        }, 
+        "opB":{
+          "NomeObjeto":"OPÇÃO B", 
+          "Imagem": "../../../../Assets/Image/Estacoes/bone.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/Jogo02/opB.mp3",
+          "Flag": false, 
+          "width":190, 
+          "height":200
+        }, 
+        "opC":{
+          "NomeObjeto":"OPÇÃO C", 
+          "Imagem": "../../../../Assets/Image/Estacoes/oculos.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/file.mp3",
+          "Flag": false, 
+          "width":190, 
+          "height":200
+        }
+  
+      }
+    },
+
+    {
+       "ID":"Jogo03",
+      "Imagem":"../../../../Assets/Image/Estacoes/neve.jpg",
+      "Width": 200, 
+      "Height":300,
+      "Title":"QUAL ROUPA SE USA NO INVERNO?",
+      "Pergunta": "../../../../Assets/Audio/Estacoes/Jogo04/file.mp3", 
+      "Opcoes":{
+        "opA":{
+          "NomeObjeto":"A) CAMISETA 1", 
+          "Imagem": "../../../../Assets/Image/Estacoes/camiseta1.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/Jogo04/file.mp3",
+          "Flag": false, 
+          "width":170, 
+          "height":180
+        }, 
+        "opB":{
+          "NomeObjeto":"B) BLUSÃO", 
+          "Imagem": "../../../../Assets/Image/Estacoes/jaqueta.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/Jogo04/file.mp3",
+          "Flag": true, 
+          "width":170, 
+          "height":171
+        }, 
+        "opC":{
+          "NomeObjeto":" C) CAMISETA 2", 
+          "Imagem": "../../../../Assets/Image/Estacoes/camiseta2.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Estacoes/Jogo04/file.mp3",
+          "Flag": false,
+          "width":170, 
+          "height":171
+        }
+  
+      }
+    }, 
+    
+  ]
