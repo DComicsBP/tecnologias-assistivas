6e7a3ad8e6d0502a52fe5ac5a9060cb69bc796f7
Daione <daione.pavan@operacao.rcadigital.com.br>
2018-10-25 19:42:02 -0200

    mais imagens sobre rotinas

--- Components/HtmlComponnents/Rotinas-II/jogo-itens.js
+++ Components/HtmlComponnents/Rotinas-II/jogo-itens.js
@@ -226,6 +226,116 @@ var itens = [
             }
 
         ]
+    },
+    {
+        "ID": "JOGO01",
+        "narracao": "",
+        "descricao": "",
+        "Imagens": [
+            {
+                "Imagem": {
+                     "ID": "01" ,
+                     "src": "../../../Assets/Image/Animais/baleiaJubarte.png" ,
+                     "width": "" ,
+                     "height": "" ,
+                     "narracao": "" ,
+                     "descricao": ""
+                }
+            },
+            {
+                "Imagem": {
+                    "ID": "02" ,
+                    "src": "../../../Assets/Image/Animais/baleiaJubarte.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": ""
+               }
+            },
+            {
+                "Imagem": {
+                    "ID": "03" ,
+                    "src": "../../../Assets/Image/Animais/baleiaJubarte.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": ""
+               }
+            },
+            {
+                "Imagem": {
+                    "ID": "04" ,
+                    "src": "../../../Assets/Image/Animais/baleiaJubarte.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": ""
+               }
+            }
+        ],
+
+        "grupos": [
+            {
+                
+                "opcoes": [
+                    {
+                        "op": {
+                            
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": true ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op1A" ,
+                        }
+                    },
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": false ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op1B" ,
+                        }
+                    },
+                    
+                ], 
+                    "class":"grupoA"
+            }, 
+
+            {
+                "opcoes": [
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": true ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op2A" ,
+                        }
+                    },
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": false ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op2A" ,
+                        }
+                    }
+                    
+                ], 
+                "class":"grupoB"
+            }
+
+        ]
     }
+
     
 ]
