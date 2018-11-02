30d582c8c3f8dda7f19206811d64f806755afc7c
DComicsBP <dayonne.p@gmail.com>
2018-11-02 07:35:02 -0300

    restruturando o projeto

--- Components/HtmlComponnents/Rotinas/Rotinas-II/jogo-itens.js
+++ Components/HtmlComponnents/Rotinas/Rotinas-II/jogo-itens.js
@@ -2,48 +2,37 @@ var itens = [
     {
         "ID": "JOGO01",
         "narracao": "",
-        "descricao": "",
+        "descricao": "QUANDO VOCÊ CHEGA NA ESCOLA VOCÊ ESPERA O SEU COLEGA, SE DESPEDE DA MAMÃE, VAI PARA A SALA DE AULA COM A PROFESSORA. DEPOIS VOCÊ",
         "Imagens": [
             {
-                "Imagem": [
-                    { "ID": "01" },
-                    { "src": "../../../Assets/Image/Animais/baleiaJubarte.png" },
-                    { "width": "" },
-                    { "height": "" },
-                    { "narracao": "" },
-                    { "descricao": "" }
-                ]
+                "Imagem": {
+                     "ID": "01" ,
+                     "src": "../../../Assets/Image/Rotina/Entrada/Entrada.jpg" ,
+                     "width": "100" ,
+                     "height": "100" ,
+                     "narracao": "" ,
+                     "descricao": ""
+                }
             },
             {
-                "Imagem": [
-
-                    { "ID": "01" },
-                    { "src": "../../../Assets/Image/Animais/baleiaJubarte.png" },
-                    { "width": "" },
-                    { "height": "" },
-                    { "narracao": "" },
-                    { "descricao": "" }
-                ]
+                "Imagem": {
+                    "ID": "02" ,
+                    "src": "../../../Assets/Image/Rotina/Entrada/tchau a mamae.jpeg" ,
+                    "width": "100" ,
+                    "height": "100" ,
+                    "narracao": "" ,
+                    "descricao": ""
+               }
             },
             {
-                "Imagem": [
-                    { "ID": "03" },
-                    { "src": "../../../Assets/Image/Animais/baleiaJubarte.png" },
-                    { "width": "" },
-                    { "height": "" },
-                    { "narracao": "" },
-                    { "descricao": "" }
-                ]
-            },
-            {
-                "Imagem": [
-                    { "ID": "04" },
-                    { "src": "../../../Assets/Image/Animais/baleiaJubarte.png" },
-                    { "narracao": "" },
-                    { "width": "" },
-                    { "height": "" },
-                    { "descricao": "" }
-                ]
+                "Imagem": {
+                    "ID": "03" ,
+                    "src": "../../../Assets/Image/Rotina/Entrada/salas.jpeg" ,
+                    "width": "100" ,
+                    "height": "100" ,
+                    "narracao": "" ,
+                    "descricao": ""
+               }
             }
         ],
 
@@ -52,58 +41,57 @@ var itens = [
                 
                 "opcoes": [
                     {
-                        "op": [
-                            
-                            { "src": "../../../Assets/Image/Animais/buffalo.png" },
-                            { "flag": true },
-                            { "width": "" },
-                            { "height": "" },
-                            { "narracao": "" },
-                            { "id": "op1A" },
-
-                        ]
+                        
+                        "op": {
+                            "src": "../../../Assets/Image/Rotina/Entrada/salas.jpeg" ,
+                            "flag": true ,
+                            "width": "100" ,
+                            "height": "100" ,
+                            "descricao": "GUARDA A MOCHILA",
+                            "narracao": "" ,
+                            "id": "op1A" ,
+                        }
                     },
                     {
-                        "op": [
+                        "op": {
 
-                            { "src": "../../../Assets/Image/Animais/buffalo.png" },
-                            { "flag": false },
-                            { "width": "" },
-                            { "height": "" },
-                            { "narracao": "" },
-                            { "id": "op1B" },
-
-                        ]
+                            "src": "../../../Assets/Image/Rotina/Entrada/higiene.jpg" ,
+                            "flag": false ,
+                            "width": "100" ,
+                            "height": "100" ,
+                            "narracao": "" ,
+                            "descricao": "FAZ A HIGIENE",
+                            "id": "op1B" ,
+                        }
                     },
                     
                 ], 
                     "class":"grupoA"
             }, 
+
             {
                 "opcoes": [
                     {
-                        "op": [
+                        "op": {
 
-                            { "src": "../../../Assets/Image/Animais/cachorro.png" },
-                            { "flag": false },
-                            { "width": "" },
-                            { "height": "" },
-                            { "narracao": "" },
-                            { "id": "op2A" },
-
-                        ]
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": true ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op2A" ,
+                        }
                     },
                     {
-                        "op": [
+                        "op": {
 
-                            { "src": "../../../Assets/Image/Animais/cachorro.png" },
-                            { "flag": true },
-                            { "width": "" },
-                            { "height": "" },
-                            { "narracao": "" },
-                            { "narracao": "op2B" },
-
-                        ]
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": false ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op2A" ,
+                        }
                     }
                     
                 ], 
@@ -112,5 +100,235 @@ var itens = [
 
         ]
     },
+-
+    {
+        "ID": "JOGO02",
+        "narracao": "",
+        "descricao": "DESCRICAO 2",
+        "Imagens": [
+            {
+                "Imagem": {
+                     "ID": "01",
+                     "src": "../../../Assets/Image/Animais/galinha.png" ,
+                     "width": "" ,
+                     "height": "" ,
+                     "narracao": "" ,
+                     "descricao": "" 
+                }
+            },
+
+            {
+                "Imagem": {
+
+                    "ID": "02" ,
+                    "src": "../../../Assets/Image/Animais/galinha.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": "" 
+                }
+            },
+
+            {
+                "Imagem": {
+
+                    "ID": "03" ,
+                    "src": "../../../Assets/Image/Animais/galinha.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": "" 
+                }
+            },
+
+            {
+                "Imagem": {
+
+                    "ID": "04" ,
+                    "src": "../../../Assets/Image/Animais/galinha.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": "" 
+                }
+            }
+        ],
+
+        "grupos": [
+            {
+                
+                "opcoes": [
+                    {
+                        "op": {
+                            
+                            "src": "../../../Assets/Image/Animais/elefante.png" ,
+                            "flag": false,
+                            "width": "",
+                            "height": "",
+                            "narracao": "",
+                            "id": "op1A",
+                        }
+                    },
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/elefante.png" ,
+                            "flag": true,
+                            "width": "",
+                            "height": "",
+                            "narracao": "",
+                            "id": "op1B",
+
+                        }
+                    },
+                    
+                ], 
+                    "class":"grupoA"
+            }, 
+
+            {
+                "opcoes": [
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/jacare.png" ,
+                            "flag": true,
+                            "width": "",
+                            "height": "",
+                            "narracao": "", 
+                            "id": "op2A",
+
+                        }
+                    },
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/jacare.png" ,
+                            "flag": false,
+                            "width": "",
+                            "height": "",
+                            "narracao": "",
+                            "id": "op2B",
+
+                        }
+                    }
+                    
+                ], 
+                "class":"grupoB"
+            }
+
+        ]
+    },
+
+    {
+        "ID": "JOGO01",
+        "narracao": "",
+        "descricao": "",
+        "Imagens": [
+            {
+                "Imagem": {
+                     "ID": "01" ,
+                     "src": "../../../Assets/Image/Animais/baleiaJubarte.png" ,
+                     "width": "" ,
+                     "height": "" ,
+                     "narracao": "" ,
+                     "descricao": ""
+                }
+            },
+            {
+                "Imagem": {
+                    "ID": "02" ,
+                    "src": "../../../Assets/Image/Animais/baleiaJubarte.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": ""
+               }
+            },
+            {
+                "Imagem": {
+                    "ID": "03" ,
+                    "src": "../../../Assets/Image/Animais/baleiaJubarte.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": ""
+               }
+            },
+            {
+                "Imagem": {
+                    "ID": "04" ,
+                    "src": "../../../Assets/Image/Animais/baleiaJubarte.png" ,
+                    "width": "" ,
+                    "height": "" ,
+                    "narracao": "" ,
+                    "descricao": ""
+               }
+            }
+        ],
+
+        "grupos": [
+            {
+                
+                "opcoes": [
+                    {
+                        "op": {
+                            
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": true ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op1A" ,
+                        }
+                    },
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": false ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op1B" ,
+                        }
+                    },
+                    
+                ], 
+                    "class":"grupoA"
+            }, 
+
+            {
+                "opcoes": [
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": true ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op2A" ,
+                        }
+                    },
+                    {
+                        "op": {
+
+                            "src": "../../../Assets/Image/Animais/buffalo.png" ,
+                            "flag": false ,
+                            "width": "" ,
+                            "height": "" ,
+                            "narracao": "" ,
+                            "id": "op2A" ,
+                        }
+                    }
+                    
+                ], 
+                "class":"grupoB"
+            }
+
+        ]
+    }
+
     
 ]
