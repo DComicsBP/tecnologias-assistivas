5ccfe8a934007cb98c69622f5086a451a5d89d01
DComicsBP <dayonne.p@gmail.com>
2018-11-02 06:58:12 -0300

    um commit

--- Components/HtmlComponnents/Estacoes/jogo-itens.js
+++ Components/HtmlComponnents/Estacoes/jogo-itens.js
@@ -106,5 +106,63 @@ var itens = [
   
       }
     }, 
+    {
+      "ID":"Jogo01",
+      "Title":"QUAL IMAGEM REPRESENTA MELHOR O OUTONO?",
+      "Pergunta": "../../../../Assets/Audio/Estacoes/Jogo01/roupa_verao.mp3", 
+      "Opcoes":{
+        "opA":{
+          "NomeObjeto":"A) ", 
+          "Imagem": "../../../../Assets/Image/Estacoes/outono.jpg",
+          "Flag": true, 
+          "width":330, 
+          "height":330
+        }, 
+        "opB":{
+          "NomeObjeto":"B) ", 
+          "Imagem": "../../../../Assets/Image/Estacoes/imagem_aleatoria3.jpg",
+          "Flag": false, 
+          "width":250, 
+          "height":250
+        }, 
+        "opC":{
+          "NomeObjeto":" C) ", 
+          "Imagem": "../../../../Assets/Image/Estacoes/imagem_aleatoria4.jpg",
+          "Flag": false,
+          "width":250, 
+          "height":250
+        }
+  
+      }
+    },
+    {
+      "ID":"Jogo01",
+      "Title":"QUAL IMAGEM REPRESENTA MELHOR A PRIMAVERA?",
+      "Pergunta": "../../../../Assets/Audio/Estacoes/Jogo01/roupa_verao.mp3", 
+      "Opcoes":{
+        "opA":{
+          "NomeObjeto":"A) ", 
+          "Imagem": "../../../../Assets/Image/Estacoes/imagem_aleatoria.jpg",
+          "Flag": false, 
+          "width":330, 
+          "height":330
+        }, 
+        "opB":{
+          "NomeObjeto":"B) ", 
+          "Imagem": "../../../../Assets/Image/Estacoes/imagem_aleatoria2.jpg",
+          "Flag": false, 
+          "width":250, 
+          "height":250
+        }, 
+        "opC":{
+          "NomeObjeto":" C) ", 
+          "Imagem": "../../../../Assets/Image/Estacoes/parque.jpg",
+          "Flag": true,
+          "width":250, 
+          "height":250
+        }
+  
+      }
+    }
     
   ]
