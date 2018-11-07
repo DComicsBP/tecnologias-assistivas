38c6751a866c0a02c2abb0aedfc41c6c90030c55
DAIONE CAROLINE BALDUZZI PAVAN <DAIONE CAROLINE BALDUZZI PAVAN>
2018-11-07 21:54:43 -0200

    um commit

---
+++ Components/HtmlComponnents/Letras/jogo-itens.js
@@ -0,0 +1,168 @@
+var itens = [
+    {
+        "Imagem":"../../../Assets/Image/Letras/a.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/b.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/c.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/d.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/e.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/f.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/g.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/h.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/i.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/j.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/k.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/l.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/m.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/n.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/o.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/p.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/q.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/r.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/s.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/t.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/u.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/v.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/w.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/x.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    {
+        "Imagem":"../../../Assets/Image/Letras/y.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }, 
+    
+    {
+        "Imagem":"../../../Assets/Image/Letras/z.jpg",
+        "Width":"15%",
+        "Height":"15%", 
+        "Audio": "../../../Assets/Audio/Som_Letras/"
+    }
+
+
+
+
+
+
+
+
+
+]
