cda61838c98a6ac0c48983c49bb94cee47079d0c
DaioneB <dcbpavan@restinga.ifrs.edu.br>
2018-11-18 20:18:12 -0200

    mais modificacoes

---
+++ Components/HtmlComponnents/Animais/info-jogo.js
@@ -0,0 +1,128 @@
+var itens = [
+  {
+    "id": "burro",
+    "nomeAnimal": "BURRO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Burro.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Burro.mp3",
+  },
+  {
+    "id": "cabra",
+    "nomeAnimal": "CABRA",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Cabra.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Cabra.mp3",
+  },
+  {
+    "id": "cachorroGrande",
+    "nomeAnimal": "CACHORRO GRANDE",
+    "imagemAnimal": "../../../Assets/Animais_Fotos/",
+    "audio": "../../../Assets/Audio/Animais_Sons/Cabra.mp3",
+  },
+  {
+    "id": "canarioBelga",
+    "nomeAnimal": "CANÁRIO BELGA",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Canário Belga.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Canário Belga.mp3",
+  },
+  {
+    "id": "cavalo",
+    "nomeAnimal": "CAVALO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Cavalo.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Cavalo.mp3",
+  },
+  {
+    "id": "galinha",
+    "nomeAnimal": "GALINHA",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Galinha.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Galinha.mp3",
+  },
+  {
+    "id": "galo",
+    "nomeAnimal": "GALO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Galo.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Galo.mp3",
+  },
+  {
+    "id": "gatinho",
+    "nomeAnimal": "GATINHO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Gatinho.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Gatinho.mp3",
+  },
+  {
+    "id": "gatoGrande",
+    "nomeAnimal": "GATO GRANDE",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Cachorro Grande.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Gato Grande.mp3",
+  },
+  {
+    "id": "grilo",
+    "nomeAnimal": "GRILO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Grilo.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Grilo.mp3",
+  }, 
+  {
+    "id": "leao",
+    "nomeAnimal": "LEÃO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Lobo.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Leão.mp3",
+  }, {
+    "id": "lobo",
+    "nomeAnimal": "LOBO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Lobo.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Lobos.mp3",
+  }, {
+    "id": "macaco",
+    "nomeAnimal": "MACACO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Macaco.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Macaco.mp3",
+  }, 
+  {
+    "id": "morcego",
+    "nomeAnimal": "MORCEGO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Morcego.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Morcegos.mp3",
+  }, 
+  {
+    "id": "ovelha",
+    "nomeAnimal": "OVELHA",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Ovelha.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Ovelha.mp3",
+  },
+  {
+    "id": "pato",
+    "nomeAnimal": "PATO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Pato.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Pato.mp3",
+  },
+  {
+    "id": "porco",
+    "nomeAnimal": "PORCO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Porco.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Porco.mp3",
+  },
+  {
+    "id": "sapo",
+    "nomeAnimal": "SAPO",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Sapo.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Sapos.mp3",
+  },
+  {
+    "id": "serpente",
+    "nomeAnimal": "SERPENTE",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Serpente.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Serpente.mp3",
+  },
+  {
+    "id": "tigre",
+    "nomeAnimal": "TIGRE",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Tigre.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Tigre.mp3",
+  },
+  {
+    "id": "vaca",
+    "nomeAnimal": "VACA",
+    "imagemAnimal": "../../../Assets/Image/Animais_Fotos/Vaca.jpg",
+    "audio": "../../../Assets/Audio/Animais_Sons/Vaca.mp3",
+  },
+
+
+]
