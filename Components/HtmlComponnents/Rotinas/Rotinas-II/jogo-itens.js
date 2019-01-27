9512324eff428fbf2895e33513ad7264e26b75c4
DComicsBP <dayonne.p@gmail.com>
2019-01-27 15:17:09 -0200

    arrumando a parte das rotinas

--- Components/HtmlComponnents/Rotinas/Rotinas-II/jogo-itens.js
+++ Components/HtmlComponnents/Rotinas/Rotinas-II/jogo-itens.js
@@ -1,334 +1,43 @@
 var itens = [
     {
-        "ID": "JOGO01",
-        "narracao": "",
-        "descricao": "QUANDO VOCÊ CHEGA NA ESCOLA VOCÊ ESPERA O SEU COLEGA, SE DESPEDE DA MAMÃE, VAI PARA A SALA DE AULA COM A PROFESSORA. DEPOIS VOCÊ",
-        "Imagens": [
-            {
-                "Imagem": {
-                     "ID": "01" ,
-                     "src": "../../../../Assets/Image/Rotina/Entrada/Entrada.jpg" ,
-                     "width": "100" ,
-                     "height": "100" ,
-                     "narracao": "" ,
-                     "descricao": ""
-                }
-            },
-            {
-                "Imagem": {
-                    "ID": "02" ,
-                    "src": "../../../../Assets/Image/Rotina/Entrada/tchau a mamae.jpeg" ,
-                    "width": "100" ,
-                    "height": "100" ,
-                    "narracao": "" ,
-                    "descricao": ""
-               }
-            },
-            {
-                "Imagem": {
-                    "ID": "03" ,
-                    "src": "../../../../Assets/Image/Rotina/Entrada/salas.jpeg" ,
-                    "width": "100" ,
-                    "height": "100" ,
-                    "narracao": "" ,
-                    "descricao": ""
-               }
-            }
-        ],
-
-        "grupos": [
-            {
-                
-                "opcoes": [
-                    {
-                        
-                        "op": {
-                            "src": "../../../../Assets/Image/Rotina/Entrada/salas.jpeg" ,
-                            "flag": true ,
-                            "width": "100" ,
-                            "height": "100" ,
-                            "descricao": "GUARDA A MOCHILA",
-                            "narracao": "" ,
-                            "id": "op1A" ,
-                        }
-                    },
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Rotina/Entrada/higiene.jpg" ,
-                            "flag": false ,
-                            "width": "100" ,
-                            "height": "100" ,
-                            "narracao": "" ,
-                            "descricao": "FAZ A HIGIENE",
-                            "id": "op1B" ,
-                        }
-                    },
-                    
-                ], 
-                    "class":"grupoA"
-            }, 
-
-            {
-                "opcoes": [
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Animais/buffalo.png" ,
-                            "flag": true ,
-                            "width": "" ,
-                            "height": "" ,
-                            "narracao": "" ,
-                            "id": "op2A" ,
-                        }
-                    },
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Animais/buffalo.png" ,
-                            "flag": false ,
-                            "width": "" ,
-                            "height": "" ,
-                            "narracao": "" ,
-                            "id": "op2A" ,
-                        }
-                    }
-                    
-                ], 
-                "class":"grupoB"
-            }
-
-        ]
-    },
--
-    {
-        "ID": "JOGO02",
-        "narracao": "",
-        "descricao": "DESCRICAO 2",
-        "Imagens": [
-            {
-                "Imagem": {
-                     "ID": "01",
-                     "src": "../../../../Assets/Image/Animais/galinha.png" ,
-                     "width": "" ,
-                     "height": "" ,
-                     "narracao": "" ,
-                     "descricao": "" 
-                }
-            },
-
-            {
-                "Imagem": {
-
-                    "ID": "02" ,
-                    "src": "../../../../Assets/Image/Animais/galinha.png" ,
-                    "width": "" ,
-                    "height": "" ,
-                    "narracao": "" ,
-                    "descricao": "" 
-                }
-            },
-
-            {
-                "Imagem": {
-
-                    "ID": "03" ,
-                    "src": "../../../../Assets/Image/Animais/galinha.png" ,
-                    "width": "" ,
-                    "height": "" ,
-                    "narracao": "" ,
-                    "descricao": "" 
-                }
-            },
-
-            {
-                "Imagem": {
-
-                    "ID": "04" ,
-                    "src": "../../../../Assets/Image/Animais/galinha.png" ,
-                    "width": "" ,
-                    "height": "" ,
-                    "narracao": "" ,
-                    "descricao": "" 
-                }
-            }
-        ],
-
-        "grupos": [
-            {
-                
-                "opcoes": [
-                    {
-                        "op": {
-                            
-                            "src": "../../../../Assets/Image/Animais/elefante.png" ,
-                            "flag": false,
-                            "width": "",
-                            "height": "",
-                            "narracao": "",
-                            "id": "op1A",
-                        }
-                    },
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Animais/elefante.png" ,
-                            "flag": true,
-                            "width": "",
-                            "height": "",
-                            "narracao": "",
-                            "id": "op1B",
-
-                        }
-                    },
-                    
-                ], 
-                    "class":"grupoA"
-            }, 
-
-            {
-                "opcoes": [
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Animais/jacare.png" ,
-                            "flag": true,
-                            "width": "",
-                            "height": "",
-                            "narracao": "", 
-                            "id": "op2A",
-
-                        }
-                    },
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Animais/jacare.png" ,
-                            "flag": false,
-                            "width": "",
-                            "height": "",
-                            "narracao": "",
-                            "id": "op2B",
-
-                        }
-                    }
-                    
-                ], 
-                "class":"grupoB"
-            }
-
-        ]
-    },
-
-    {
-        "ID": "JOGO01",
-        "narracao": "",
-        "descricao": "",
-        "Imagens": [
-            {
-                "Imagem": {
-                     "ID": "01" ,
-                     "src": "../../../../Assets/Image/Animais/baleiaJubarte.png" ,
-                     "width": "" ,
-                     "height": "" ,
-                     "narracao": "" ,
-                     "descricao": ""
-                }
-            },
-            {
-                "Imagem": {
-                    "ID": "02" ,
-                    "src": "../../../../Assets/Image/Animais/baleiaJubarte.png" ,
-                    "width": "" ,
-                    "height": "" ,
-                    "narracao": "" ,
-                    "descricao": ""
-               }
-            },
-            {
-                "Imagem": {
-                    "ID": "03" ,
-                    "src": "../../../../Assets/Image/Animais/baleiaJubarte.png" ,
-                    "width": "" ,
-                    "height": "" ,
-                    "narracao": "" ,
-                    "descricao": ""
-               }
-            },
-            {
-                "Imagem": {
-                    "ID": "04" ,
-                    "src": "../../../../Assets/Image/Animais/baleiaJubarte.png" ,
-                    "width": "" ,
-                    "height": "" ,
-                    "narracao": "" ,
-                    "descricao": ""
-               }
-            }
-        ],
-
-        "grupos": [
-            {
-                
-                "opcoes": [
-                    {
-                        "op": {
-                            
-                            "src": "../../../../Assets/Image/Animais/buffalo.png" ,
-                            "flag": true ,
-                            "width": "" ,
-                            "height": "" ,
-                            "narracao": "" ,
-                            "id": "op1A" ,
-                        }
-                    },
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Animais/buffalo.png" ,
-                            "flag": false ,
-                            "width": "" ,
-                            "height": "" ,
-                            "narracao": "" ,
-                            "id": "op1B" ,
-                        }
-                    },
-                    
-                ], 
-                    "class":"grupoA"
-            }, 
-
-            {
-                "opcoes": [
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Animais/buffalo.png" ,
-                            "flag": true ,
-                            "width": "" ,
-                            "height": "" ,
-                            "narracao": "" ,
-                            "id": "op2A" ,
-                        }
-                    },
-                    {
-                        "op": {
-
-                            "src": "../../../../Assets/Image/Animais/buffalo.png" ,
-                            "flag": false ,
-                            "width": "" ,
-                            "height": "" ,
-                            "narracao": "" ,
-                            "id": "op2A" ,
-                        }
-                    }
-                    
-                ], 
-                "class":"grupoB"
-            }
-
-        ]
+      "ID":"Jogo01",
+      "IDModal":"Jogo01M",
+      "IDJogoDiv":"JogoDiv01",
+      "Imagem":"../../../../Assets/Image/Transito/semáfaro.png",
+      "Width": 200, 
+      "Height":300,
+      "Title":"VOCÊ SÓ PODERÁ ATRAVESSAR A RUA SE O SINAL ESTIVER COM QUE COR?",
+      "Pergunta": "../../../../Assets/Audio/Transito/partida01/questao.mp3", 
+      "Opcoes":{
+        "opA":{
+          "NomeObjeto":"A) SINAL VERDE", 
+          "Imagem": "../../../../Assets/Image/Transito/sinalVerde.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Transito/partida01/opA.mp3",
+          "Modal":"../../../../Assets/Image/DepoisDasRespostas/ramtaro.gif",
+          "Flag": false, 
+          "width":63, 
+          "height":150
+        }, 
+        "opB":{
+          "NomeObjeto":"B) SINAL VERMELHO", 
+          "Imagem": "../../../../Assets/Image/Transito/sinalVermelho.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Transito/partida01/opB.mp3",
+          "Modal":"../../../../Assets/Image/DepoisDasRespostas/happy.gif ",
+          "Flag": true, 
+          "width":70, 
+          "height":150
+        }, 
+        "opC":{
+          "NomeObjeto":" C) SINAL AMARELO", 
+          "Imagem": "../../../../Assets/Image/Transito/sinalAmarelo.jpg",
+          "SomNarracao": "../../../../Assets/Audio/Transito/partida01/opC.mp3",
+          "Modal":"../../../../Assets/Image/DepoisDasRespostas/ramtaro.gif",
+          "Flag": false,
+          "width":70, 
+          "height":150
+        }
+  
+      }
     }
-
-    
-]
+     
+  ]
--- Components/HtmlComponnents/Rotinas/Rotinas-II/jogo.js
+++ Components/HtmlComponnents/Rotinas/Rotinas-II/jogo.js
@@ -1,102 +1,121 @@
-var j = 0//Math.floor(Math.random() * 3) + 0;
-var dados = '';
-var opcoes = '';
+var count = 0;
+let i = 0;
 
-var rotinas = function (j) {
+var checkSection = function () {
 
-    var carregaDados = function (j) {
+}
+var regrasTransito = function () {
+
+
+    this.init = function () {
+
+        itens.forEach(item => {
+            carregaDados(item);
+
+        });
+    }
+
+    function carregaDados(item) {
+        let opcaoC = "";
+        if (typeof item.Opcoes.opC !== "undefined") {
+
+            opcaoC = `<div class="card questionCard opC" style="width: 18rem; cursor: pointer;" id="opCBox" data-flag="${item.Opcoes.opC.Flag}" data-modal ="${item.Opcoes.opC.Modal}" onclick="checkValue(this)" onmouseenter="onMouseEnter(this)" onmouseleave="onMouseLeave(this)">
+                                <audio id="opCAudio${i}" controls  src="${item.Opcoes.opC.SomNarracao}" hidden>
+                                </audio>    
+                                <h4 id="opCTexto">${item.Opcoes.opC.NomeObjeto}</h4>
+                                <img class="" src="${item.Opcoes.opC.Imagem}" height="${item.Opcoes.opC.height}" width="${item.Opcoes.opC.width}" id="opC">
+                                <br>
+                                <br>
+                            </div>`;
+
+        }
+
+        let imagemPrincipal =
+            `<div class="card" style="width: 28rem;">
+                <img class="card-img-top" src="${item.Imagem}" id="principalImage" height="${item.Height}" width="${item.Width}"> 
+             <div />`;
+
+        if (typeof item.Imagem === "undefined" || item.Imagem === "" || item.Imagem === null) {
+            imagemPrincipal = "";
+        }
+        var display = 'none';
+        if (count == 0) {
+            display = 'block';
+        }
+        count++;
+
+        var dados = `
+    <br /> <br />
+    <section id="${item.ID}" data-indice = "${i}" style="display:${display};">
+        <div class="container" id="${item.IDJogoDiv}">
+            <div class="row" id="ToHide${i}">
+            <div class="card-body">
+            <div class="">
+                <audio id="pergunta${i}"  src="${item.Pergunta}" controls></audio>
+
+                <h1 id="title">${item.Title}</h1>
+                    `+ imagemPrincipal + `
+            </div>
+                    <br>
+                    <br>
+                    <div style="display: table-row" class="card-columns options">
+                        <div class="card questionCard opA" style="width: 18rem; cursor: pointer" id="opABox" data-flag="${item.Opcoes.opA.Flag}" data-modal ="${item.Opcoes.opA.Modal}" onclick="checkValue(this)" onmouseenter="onMouseEnter(this)" onmouseleave="onMouseLeave(this)">
+                            <h4 id="titleA" id="opATexto">${item.Opcoes.opA.NomeObjeto}</h4>
+                            <img class="" src="${item.Opcoes.opA.Imagem}" id="opA" height="${item.Opcoes.opA.height}" width="${item.Opcoes.opA.width}">
+                            <audio id="opAAudio${i}" controls="controls" src="${item.Opcoes.opA.SomNarracao}" hidden></audio>
+                            <br>
+                            <br>
+                        </div>
+                        <div class="card questionCard opB" style="width: 18rem; cursor: pointer;" id="opBBox" data-flag="${item.Opcoes.opB.Flag}" data-modal ="${item.Opcoes.opB.Modal}" onclick="checkValue(this)" onmouseenter="onMouseEnter(this)" onmouseleave="onMouseLeave(this)">
+                            <audio id="opBAudio${i}" controls src="${item.Opcoes.opB.SomNarracao}" hidden>
+                            </audio>
+                            <h4 id="opBTexto">${item.Opcoes.opB.NomeObjeto}</h4>
+                            <img class="" src="${item.Opcoes.opB.Imagem}" height="${item.Opcoes.opB.height}" width="${item.Opcoes.opB.width}" id="opB">
+                            <br>
+                            <br>
+                        </div>` + opcaoC + ` 
+                    </div>
+                </div>
+            </div>
+        </div>
         
-        dados += `<div id="${itens[j].ID}" class="row">`;
+    <div id="modals${i}"> 
+        <div id="${item.IDModal}_TRUE${i}" style="display:none;" data-flag="true" class="myModal"> 
+            <div>
+                <h1>PARABÉNS!, JOGUEA PRÓXIMA PARTIDA</h1>
+                <img src="../../../Assets/Image/DepoisDasRespostas/aligator.gif">
+                <audio src="" id="modalTrue${i}"></audio>  
+                <br />
+                <br />
+                <button type="button" class="btn btn-success" id="voltar${i}" onclick="renderizaPartidaPosterior(event)"> CONTINUAR -> </button> 
+            </div>
+        </div>
+        
+        <div id="${item.IDModal}_FALSE${i}" style="display:none;" data-flag="false" class="myModal"> 
+            <div>
+                <h1>NÃO DESISTA, TENTE NOVAMENTE!</h1>
+                <img src="../../../Assets/Image/DepoisDasRespostas/aligator.gif" id="">
+                <audio src="" id="modalFalse${i}"></audio>
+                <br />
+                <br />
+                <button type="button" class="btn btn-secondary" id="voltar${i}" onclick="renderizaPartidaAnterior(event)"> <- VOLTAR</button> 
+            </div>
+        </div>
+    <div/>
+    </section>  
+    `;
 
-        const element = itens[j];
+        i++;
 
-        for (let k = 0; k < element.Imagens.length; k++) {
+        $("#render").append(dados.trim());
 
-            const images = element.Imagens[k];
-      
 
-                dados += `<div class="col-sm-2" id="${images.Imagem.ID}">`;
-                dados += `<div class="card" id="${images.Imagem.ID}">`;
-                dados += `<div class="card-body">`;
-                dados += `<audio src="${images.Imagem.narracao}"></audio>`;
-                dados += ` <img src="${images.Imagem.src}"  height="${images.Imagem.height}" width="${images.Imagem.width}" alt="">`;
-                dados += `</div>`;
-                dados += `</div>`;
-                dados += `</div>`;
-
-        }
-
-        for (let k = 0; k < element.grupos.length; k++) {
-            const ops = element.grupos[k];
-            
-            for (let l = 0; l < ops.opcoes.length; l++) {
-                const op = ops.opcoes[l];
-                opcoes += `<div class="${ops.class} col-sm-2">`;
-                opcoes+= `<div class="card"> `;
-                opcoes+= `<div class="card-body"> `;
-                opcoes += `<a href='#' class="${ops.class}" id="${op.op.id}" data="${op.op.flag}" onclick="remove(event)">`
-                opcoes +=   `<div data="${op.op.flag}">`;
-                opcoes +=       `<img src="${op.op.src}" alt="${op.op.descricao}" height="${op.op.height}" width="${op.op.width}">`;
-                opcoes +=   `</div>`;
-                opcoes += `</a>`;
-                opcoes += `</div>`;
-                opcoes += `</div>`;
-                opcoes += `</div>`;
-
-            }
-        }
     }
-
-    this.init = function (j) {
-        carregaDados(j);
-            render(j); 
-            j++; 
-    }
-
-    init(j);
+    init();
 }
-
-rotinas(j);
-
- function render(j){
-     debugger; 
-    $("#descricao").append(itens[j].descricao); 
-    $(".opcoes").append(opcoes);
-    $(".sequencia").append(dados);
-    $(".grupoB").hide();
-
-}
-
-function remove(event){
-    debugger;
-    console.log('Event ===> ', event);
-    var flag =  $("#"+event.currentTarget.attributes.id.value).attr("data");
-
-    if ($("#"+event.currentTarget.attributes.id.value).attr("class") === 'grupoA') {
-        if (flag === 'true') {
-            $(".sequencia").append(event.currentTarget.innerHTML);
-            $(".grupoB").show();
-            $(".grupoA").hide();
-
-        }
-    } else {
-        if (flag === 'true') {
-            $(".sequencia").append(event.currentTarget.innerHTML);
-            opcoes = ""; 
-            dados = "";  
-
-            $(".grupoA").show();
-            $(".grupoB").show();
-            $(".grupoA").remove();
-            $(".grupoB").remove();
-            $(".sequencia").empty();
-            $("#descricao").empty();
-
-            init(j); 
-        }
-
-
-    }
+regrasTransito();
+var isHide = function () {
+    $(element).is(":visible");
 
 }
 var onMouseLeave = function(element){ 
@@ -112,4 +131,77 @@ var onMouseEnter = function (element) {
     $(audio)[0].play();
     
     $(element).css('background-color', '#FFCC33');
+} 
+var checkValue = function (e) {
+    console.log(e);
+
+    var modals = "";
+    var idJogo = "";
+    var root = e.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    if (typeof root.children[0].children[0] === "undefined") {
+        root = e.parentElement.parentElement.parentElement.parentElement.parentElement;
+        modals = root.children[1];
+    } else {
+        modals = root.children[0].children[1];
+
+    }
+    var jogo = root.children[0].children[0];
+    var idJogo = jogo.id;
+    var idModals = modals.id;
+    var modalTrue = modals.children[0].id;
+    var modalFalse = modals.children[1].id;
+
+    $("#" + idJogo).hide();
+    $("#" + idModals).show();
+
+    let valor = e.attributes[3].value;
+    if (valor == 'true') {
+        $("#" + modalTrue).show();
+        $("#" + modalFalse).hide();
+    } else {
+        $("#" + modalFalse).show();
+        $("#" + modalTrue).hide();
+
+    }
+}
+var renderizaPartidaAnterior = function (event) {
+    console.log(event);
+    var path = event.path || (event.composedPath && event.composedPath());
+    var foundSection; 
+    var foundDiv;
+    path.forEach(function (element) {
+        if(element.tagName == "SECTION"){
+            foundSection = element; 
+        }
+    });
+
+    if(typeof foundSection.children[0].children[1] == "undefined"){
+        $("#"+foundSection.children[1].id ).hide();      
+    }else{
+        $("#"+foundSection.children[0].children[1].children[1].id ).hide();
+    }
+    $("#" +foundSection.children[0].children[0].id).show(); 
+}
+var renderizaPartidaPosterior = function (event) {
+    var idNextSection = "";
+    var index = 5;
+    var path = event.path || (event.composedPath && event.composedPath());
+
+    if (path[5].nextElementSibling.nextElementSibling == null || typeof path[5].nextElementSibling.nextElementSibling === "undefined") {
+        if (path[4].nextElementSibling == null) {
+            idNextSection = path[5].nextElementSibling.id;
+        } else {
+            idNextSection = path[4].nextElementSibling.nextElementSibling.nextElementSibling.id;
+        }
+
+        index = 4;
+    } else {
+        idNextSection = path[5].nextElementSibling.nextElementSibling.nextElementSibling.id
+    }
+    if (typeof idNextSection !== "undefined") {
+        $("#" + idNextSection).show();
+    }
+
+    var idCurrentlySection = path[index].id;
+    $("#" + idCurrentlySection).hide();
 }
